0cd9f48e1809996715387fae4f64df9d7348c24a
Cade Maw <cademaw04@gmail.com>
2024-12-01 22:32:30 -0800

    Add files via upload

--- ARO 3011/ARO3011_CompAssign_R2.m
+++ ARO 3011/ARO3011_CompAssign_R2.m
@@ -1,49 +1,53 @@
 clear; clc
+
 % ===============================
 % ----- Givens -----
 % ===============================
+
 Re = 1E6;
 nuAir = 1.46E-5;
 alpha = -7*pi/180; % radians
+N = 128; % Number of panels -- MUST BE AN EVEN NUMBER
+
 
 % ===============================
 % ----- Import Airfoil Data -----
 % ===============================
 
 airfoilData = open("AirfoilData.mat").dataValues;
+leadingEdge = [0,0];
 trailingEdge = [1.02,-0.0223];
-airfoilData = [airfoilData;trailingEdge];
 
 Vo = Re*nuAir/trailingEdge(1);
-%% =
+
+
 % ======================================
 % ----- Find Panel Boundary Points -----
 % ======================================
 
-N = 128; % Number of panels -- MUST BE AN EVEN NUMBER
 TEx = trailingEdge(1); % X location of trailing edge
 
 
 %   Upper Surface
     upperSurface = airfoilData(1:102,:);
-    upperSurface = [upperSurface;trailingEdge];
+    upperSurface = [leadingEdge;upperSurface;trailingEdge];
     N_upper = N/2;
 
 %       Assign x and y
         x = zeros(length(upperSurface)+1,1); % origin as first point
         y = zeros(length(upperSurface)+1,1);
 
-        x(2:end,1) = upperSurface(:,1); 
-        y(2:end,1) = upperSurface(:,2);
+        x = upperSurface(:,1);
+        y = upperSurface(:,2);
         xBoundaryUpper = linspace(0,TEx,N_upper+1);
-
+        
 %       Linear interpolation wrt x
         yBoundaryUpper = interp1(x,y,xBoundaryUpper);
 
 
-
 %   Lower Surface
-    lowerSurface = airfoilData(103:206,:);
+    lowerSurface = airfoilData(103:end,:);
+    lowerSurface = [lowerSurface;trailingEdge];
     N_lower = N/2;
 
 %       Assign x and y
@@ -53,93 +57,65 @@ TEx = trailingEdge(1); % X location of trailing edge
 
 %       Linear interpolation wrt x
         yBoundaryLower = interp1(x,y,xBoundaryLower);
-%%
+
+
+% ==============================================================
+% ----- Re-Define Boundary and Control Points: CCW from TE -----
+% ==============================================================
+
+%   Boundary Points
+%       Upper Surface goes CW from LE - need to flip
+        xBoundaryUpper2 = fliplr(xBoundaryUpper);
+        yBoundaryUpper2 = fliplr(yBoundaryUpper);
+%       Lower Surface already goes CCW from TE
+    
+%       Remove Duplicate Point at LE
+%       Keep Duplicate Point at TE for Complete Loop
+        xBoundaryUpper2(end) = [];
+        yBoundaryUpper2(end) = [];
+        
+%       Concatenate w/ Upper first to go CCW from TE
+        xBoundary = [xBoundaryUpper2';xBoundaryLower'];
+        yBoundary = [yBoundaryUpper2';yBoundaryLower'];
+
+
 % ===============================
 % ----- Find Control Points -----
 % ===============================
 
 %   Pre-allocate control point array sizes
-    xControlUpper = zeros(1,N/2);
-    yControlUpper = zeros(1,N/2);
-    xControlLower = zeros(1,N/2);
-    yControlLower = zeros(1,N/2);
+    xControl = zeros(N,1);
+    yControl = zeros(N,1);
 
 %   Each control point is halfway between the neighboring boundary points
-    for i = 1:N/2
-        xControlUpper(i) = (xBoundaryUpper(i) + xBoundaryUpper(i+1))/2;
-        yControlUpper(i) = (yBoundaryUpper(i) + yBoundaryUpper(i+1))/2;
-    
-        xControlLower(i) = (xBoundaryLower(i) + xBoundaryLower(i+1))/2;
-        yControlLower(i) = (yBoundaryLower(i) + yBoundaryLower(i+1))/2;
+    for i = 1:N
+        xControl(i) = (xBoundary(i) + xBoundary(i+1))/2;
+        yControl(i) = (yBoundary(i) + yBoundary(i+1))/2;
     end
-%%
-% =============================================================
-% ----- Re-Define Boundary and Control Points: CW from LE -----
-% =============================================================
 
-% Important to go clockwise (CW) to make each panel's normal vector 
-% point out of the airfoil
+% ***** FLIP
+xControl = flip(xControl);
+yControl = flip(yControl);
+xBoundary = flip(xBoundary);
+yBoundary = flip(yBoundary);
 
-%   Boundary Points
-%       Upper Surface already goes CW from LE
-%       Lower Surface goes CCW from LE - need to flip
-        xBoundaryLower2 = fliplr(xBoundaryLower);
-        yBoundaryLower2 = fliplr(yBoundaryLower);
-    
-%       Remove Duplicate Points at LE and TE
-        xBoundaryLower2(1) = [];
-        xBoundaryLower2(end) = [];
-        yBoundaryLower2(1) = [];
-        yBoundaryLower2(end) = [];
-        
-%       Concatenate w/ Upper first to go CW from LE
-        xBoundary = [xBoundaryUpper';xBoundaryLower2'];
-        yBoundary = [yBoundaryUpper';yBoundaryLower2'];
-
-%   Control Points
-%       Upper Control Points already go CW from LE
-%       Lower Control Points go CCW from LE - need to flip
-        xControlLower2 = fliplr(xControlLower);
-        yControlLower2 = fliplr(yControlLower);
-
-%       Concatenate w/ Upper first to go CW from LE
-        xControl = [xControlUpper';xControlLower2'];
-        yControl = [yControlUpper';yControlLower2']; 
-%%
-% =========================================
-% ----- Find Panel Angles and Lengths -----
-% =========================================
-
-% Still going CW from LE
-
-%   Make (x,y)|N+1 = (x,y)|1 so that point N can use it & complete loop
-    xBoundary2 = xBoundary;
-    xBoundary2(N+1) = xBoundary(1);
-    yBoundary2 = yBoundary;
-    yBoundary2(N+1) = yBoundary(1);
+% =============================
+% ----- Find Panel Angles -----
+% =============================
 
 %   Pre-allocate length and angle array sizes
     theta = zeros(1,N);
     panelLength = zeros(1,N);
 
-%   Loop to calculate angle of panel wrt x-axis and length of panel
+%   Loop to calculate angle of panel wrt x-axis
     for i = 1:N
-        theta(i) = atan2( (yBoundary2(i+1)-yBoundary2(i)) , (xBoundary2(i+1)-xBoundary2(i)) ); % radians
-            % if theta(i) < 0     % Range of theta is 0 to 2pi
-            %     theta(i) = theta(i) + 2*pi;
-            % end
-        panelLength(i) = sqrt( (yBoundary2(i+1)-yBoundary2(i))^2 + (xBoundary2(i+1)-xBoundary2(i))^2 ); 
+        theta(i) = atan2( (yBoundary(i+1)-yBoundary(i)) , (xBoundary(i+1)-xBoundary(i)) ); % radians
     end
-%%
-% ================================
-% ----- Find r|ij and phi|ij -----
-% ================================
 
-%   Make (x,y)|N+1 = (x,y)|1 so that point N can use it & complete loop
-    xControl2 = xControl;
-    xControl2(N+1) = xControl(1);
-    yControl2 = yControl;
-    yControl2(N+1) = yControl(1);
+
+% ==========================
+% ----- Find r and phi -----
+% ==========================
 
 %   Pre-allocate matrices
     r = zeros(N,N);
@@ -148,11 +124,12 @@ TEx = trailingEdge(1); % X location of trailing edge
 %   For loop to calculate r|ij and phi|ij
     for i = 1:N
         for j = 1:N
-            r(i,j) = sqrt( (xControl2(j)-xControl2(i))^2 + (yControl2(j)-yControl2(i))^2 );
-            phi(i,j) = atan2( (yControl2(j)-yControl2(i)) , (xControl2(j)-xControl2(i)) );
+            r(i,j) = sqrt( (xControl(j)-xControl(i))^2 + (yControl(j)-yControl(i))^2 );
+            phi(i,j) = atan2( (yControl(j)-yControl(i)) , (xControl(j)-xControl(i)) );
         end
     end
-%%
+
+
 % =====================
 % ----- Compute q -----
 % =====================
@@ -166,7 +143,7 @@ TEx = trailingEdge(1); % X location of trailing edge
 %   Equations from Handout
     for i = 1:N
         for j = setdiff(1:N,i)    % j = 1:N but j =/= i
-            deltaS(j) = sqrt( (yBoundary2(j+1)-yBoundary2(j))^2 + (xBoundary2(j+1)-xBoundary2(j))^2 );
+            deltaS(j) = sqrt( (yBoundary(j+1)-yBoundary(j))^2 + (xBoundary(j+1)-xBoundary(j))^2 );
             C(i,j) = deltaS(j) * sin(theta(i)-phi(i,j)) / (2*pi*r(i,j));
             Cbar(i,j) = deltaS(j) * cos(theta(i)-phi(i,j)) / (2*pi*r(i,j));
         end
@@ -174,38 +151,66 @@ TEx = trailingEdge(1); % X location of trailing edge
     end
 
 q = C\B;
-%%
+
+
 % =================================
 % ----- Compute Vt, Cp, and CL-----
 % =================================
 
 %   Pre-allocate matrix sizes
-    Vt = zeros(N,1);
     Cp = zeros(N,1);
+    stuff = zeros(N,1);
+    CL1 = zeros(N,1);
 
-%   Equations from Handout
+%   Equations from Handout for Vt and Cp
     for i = 1:N
-        for j = 1:N
-            Vt(i) = Vo*cos(theta(i)-alpha) - Cbar(i,j)*q(i);
-            Cp(i) = 1 - (Vt(i)/Vo)^2;
-        end
+        stuff(i) = Vo*cos(theta(i)-alpha);
+    end
+    Vt = stuff - Cbar*q;
+
+    for i = 1:N
+        Cp(i) = 1 - (Vt(i)/Vo)^2;
     end
 
-%   Compute CL
-    for i = 1:N/2
-        CL1(i) = -Cp(i)*deltaS(i)*cos(theta(i)-alpha);
+% Compute CL
+Cp2 = Cp;
+Cp2(1:N/2) = -Cp2(1:N/2);
+    for i = 1:N
+        CL1(1) = Cp2(i)*deltaS(i)*cos(theta(i)-alpha);
     end
-    for i = N/2+1:N
-        CL2(i) = Cp(i)*deltaS(i)*cos(theta(i)-alpha);
-    end
-    CL = sum(CL1)+sum(CL2);
-%%
+CL = sum(CL1)
+
+
+
 % figure
 % hold on
-% scatter(airfoilData(:,1),airfoilData(:,2),'MarkerEdgeColor','#D4D4D4')
-% ylim([-.25 .25])
+% % plot(airfoilData(:,1),airfoilData(:,2),'MarkerEdgeColor','#D4D4D4')
+% plot(upperSurface(:,1),upperSurface(:,2),'Color','k','LineWidth',1.6)
+% plot(lowerSurface(:,1),lowerSurface(:,2),'Color','k','LineWidth',1.6)
+% grid on
+% ylim([-.3 .3])
 % xlim([-0.1 1.15])
+% xlabel('x/c')
+% ylabel('y/c')
+% title('SC (2)-0714 Airfoil with TE Extension')
 % scatter(xBoundary,yBoundary,'MarkerEdgeColor','r')
 % scatter(xControl,yControl,'MarkerEdgeColor','#77AC30')
 % hold off
-% legend('Airfoil','Boundary Points','Control Points')
+% legend('Airfoil','','Boundary Points','Control Points')
+
+% Cp(1:N/2) = flip(Cp(1:N/2));
+figure
+hold on
+plot(flip(xControl(1:N/2)),flip(Cp(1:N/2)),'DisplayName','Top')
+plot(xControl(N/2+1:end),Cp(N/2+1:end),'DisplayName','Bottom')
+legend('show')
+title('Cp vs. Zeta (CCW)')
+hold off
+axis padded
+
+% figure
+% hold on
+% plot(flip(Cp(1:N/2))) % should be flipped since we did CW from TE
+% plot(Cp(N/2+1:N))
+
+% T1 = table(xControl(N/2+1:N),Cp_top,Cp_bot);
